2c1e89b9dcd51a1073aca588f5e620392c5440c3
tomlinso <tomlinso>
2001-01-24 14:31:17 +0000

    Provide empty declaration of this class/interface to make make happy.

--- glm/src/org/cougaar/domain/mlm/ui/tpfdd/gui/view/ScheduleCellElement.java
+++ glm/src/org/cougaar/domain/mlm/ui/tpfdd/gui/view/ScheduleCellElement.java
@@ -1,2 +1,6 @@
-/* $Header: /opt/rep/cougaar/glm/glm/src/org/cougaar/domain/mlm/ui/tpfdd/gui/view/Attic/ScheduleCellElement.java,v 1.1 2000-12-15 20:17:48 mthome Exp $ */
+/* $Header: /opt/rep/cougaar/glm/glm/src/org/cougaar/domain/mlm/ui/tpfdd/gui/view/Attic/ScheduleCellElement.java,v 1.2 2001-01-24 14:31:17 tomlinso Exp $ */
 
+package org.cougaar.domain.mlm.ui.tpfdd.gui.view;
+
+public interface ScheduleCellElement {
+}
