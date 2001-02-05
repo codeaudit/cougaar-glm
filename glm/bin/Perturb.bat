e2e2976e03de6961a053713b79cf36da02c4a249
lcarlson <lcarlson>
2001-02-05 20:48:47 +0000

    updated myclasses to reflect new path

--- glm/bin/Perturb.bat
+++ glm/bin/Perturb.bat
@@ -4,7 +4,7 @@ REM calls setlibpath.bat which sets the path to the required jar files.
 CALL %ALP_INSTALL_PATH%\bin\setlibpath.bat
 
 REM runs the Asset Availability Perturbation GUI
-set MYCLASSES=ui.perturbation.asset.AssetPerturbationDisplay
+set MYCLASSES=org.cougaar.domain.mlm.ui.perturbation.asset.AssetPerturbationDisplay
 
 @ECHO ON
 
