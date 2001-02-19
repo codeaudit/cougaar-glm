93d38ce3bb4e8eb4aa9023bd789d26eb01fc92b9
jwinston <jwinston>
2001-02-19 21:26:34 +0000

    Added CountryStateCode CountryStateName and InstallationTypeCode.

--- glm/src/org/cougaar/domain/glm/xml/parser/LocationParser.java
+++ glm/src/org/cougaar/domain/glm/xml/parser/LocationParser.java
@@ -1,4 +1,4 @@
-// $Header: /opt/rep/cougaar/glm/glm/src/org/cougaar/domain/glm/xml/parser/Attic/LocationParser.java,v 1.3 2001-02-16 23:55:57 gvidaver Exp $
+// $Header: /opt/rep/cougaar/glm/glm/src/org/cougaar/domain/glm/xml/parser/Attic/LocationParser.java,v 1.4 2001-02-19 21:26:34 jwinston Exp $
 /*
  * <copyright>
  *  Copyright 1997-2000 Defense Advanced Research Projects
@@ -34,6 +34,9 @@ public class LocationParser{
     double    longitude  = Double.NaN;
     String    geoloc     = null;
     String    name       = null;
+    String    type       = null;
+    String    statecode  = null;
+    String    statename  = null;
 
     for(int i = 0; i < nlength; i++){
       Node    child      = nlist.item(i);
@@ -41,34 +44,55 @@ public class LocationParser{
       
       if(child.getNodeType() == Node.ELEMENT_NODE){
 	
-		if(childname.equals("geoloc")){
-		  Node data = child.getFirstChild();
-		  geoloc = data.getNodeValue();
-		}
-		else if(childname.equals("latitude")){
-		  Node data = child.getFirstChild();
-		  Double d = new Double(data.getNodeValue());
-		  latitude = d.doubleValue();
-		}
-		else if(childname.equals("longitude")){
-		  Node data = child.getFirstChild();
-		  Double d = new Double(data.getNodeValue());
-		  longitude = d.doubleValue();
-		}
-		else if(childname.equals("name")){
-		  Node data = child.getFirstChild();
-		  name = data.getNodeValue();
-		}
-	  }
+	if(childname.equals("geoloc")){
+	  Node data = child.getFirstChild();
+	  geoloc = data.getNodeValue();
+//  	  System.out.println("LocationParser: geoloc = " + geoloc);
 	}
+	else if(childname.equals("latitude")){
+	  Node data = child.getFirstChild();
+	  Double d = new Double(data.getNodeValue());
+	  latitude = d.doubleValue();
+//  	  System.out.println("LocationParser: latitude = " + latitude);
+	}
+	else if(childname.equals("longitude")){
+	  Node data = child.getFirstChild();
+	  Double d = new Double(data.getNodeValue());
+	  longitude = d.doubleValue();
+//  	  System.out.println("LocationParser: longitude = " + longitude);
+	}
+	else if(childname.equals("name")){
+	  Node data = child.getFirstChild();
+	  name = data.getNodeValue();
+//  	  System.out.println("LocationParser: name = " + name);
+	}
+	else if(childname.equals("type")){
+	  Node data = child.getFirstChild();
+	  type = data.getNodeValue();
+//  	  System.out.println("LocationParser: type = " + type);
+	}
+	else if(childname.equals("statecode")){
+	  Node data = child.getFirstChild();
+	  statecode = data.getNodeValue();
+//  	  System.out.println("LocationParser: name = " + statecode);
+	}
+	else if(childname.equals("statename")){
+	  Node data = child.getFirstChild();
+	  statename = data.getNodeValue();
+//  	  System.out.println("LocationParser: name = " + statename);
+	}
+      }
+    }
 	
-	  
     
     NewGeolocLocation loc = af.newGeolocLocation();
     loc.setGeolocCode(geoloc);
     loc.setLatitude(Latitude.newLatitude(latitude));
     loc.setLongitude(Longitude.newLongitude(longitude));
     loc.setName(name);
+    loc.setInstallationTypeCode(type);
+    loc.setCountryStateCode(statecode);
+    loc.setCountryStateName(statename);
     return loc;
   }
 }
