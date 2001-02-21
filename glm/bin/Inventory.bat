05550f68420809c40cc7ddd80b136c3a24b4291c
mthome <mthome>
2001-02-21 20:55:00 +0000

    Fix up scripts to use bootstrapper, etc

--- glm/bin/Inventory.bat
+++ glm/bin/Inventory.bat
@@ -2,11 +2,12 @@
 
 REM calls setlibpath.bat which sets the path to the required jar files.
 CALL %ALP_INSTALL_PATH%\bin\setlibpath.bat
+CALL %ALP_INSTALL_PATH%\bin\setarguments.bat
 
 REM produces the inventory chart display
 set MYCLASSES=org.cougaar.domain.mlm.ui.planviewer.inventory.InventoryChartUI
-
+set BS=org.cougaar.core.society.Bootstrapper
 @ECHO ON
 
-java.exe -classpath %LIBPATHS% %MYCLASSES% %1
+java.exe %MYPROPERTIES% %MYMEMORY% -classpath %LIBPATHS% %BS% %MYCLASSES% %1
 
