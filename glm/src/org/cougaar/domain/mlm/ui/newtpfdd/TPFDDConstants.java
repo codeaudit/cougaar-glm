d0dea147ff6d0c5aa54803551f83836619a19caf
wseitz <wseitz>
2001-02-23 21:19:32 +0000

    Added TPFDDConstants

---
+++ glm/src/org/cougaar/domain/mlm/ui/newtpfdd/TPFDDConstants.java
@@ -0,0 +1,13 @@
+
+package org.cougaar.domain.mlm.ui.newtpfdd;
+
+public interface TPFDDConstants {
+
+  public String SEND_HIERARCHY = "Send Hierarchy";
+  public String SEND_ASSETLOC = "Send Asset Locations";
+  public String SEND_ASSETID_TD_MAPS = "Send AssetID To Transit Data Maps";
+  public String SEND_UNIT_MANIFESTS = "Send unitManifests";
+  public String RELOAD = "RELOAD yourself";
+  public final static String ROOTNAME = "ROOT";
+
+}
