821b3d1cf67c3f90b2e31a1a6429233da655a9d1
gvidaver <gvidaver>
2001-02-24 21:46:09 +0000

    moved to query

--- glm/src/org/cougaar/domain/mlm/ui/newtpfdd/gui/view/HierarchyQuery.java
+++
@@ -1,7 +0,0 @@
-package org.cougaar.domain.mlm.ui.newtpfdd.gui.view;
-
-public class HierarchyQuery implements Query {
-    private boolean leavesOnly = false;
-    public boolean getLeavesOnly() { return leavesOnly; };
-    public void setLeavesOnly(boolean leavesOnly) { this.leavesOnly = leavesOnly; };
-}
--- glm/src/org/cougaar/domain/mlm/ui/newtpfdd/gui/view/Query.java
+++
@@ -1,7 +0,0 @@
-package org.cougaar.domain.mlm.ui.newtpfdd.gui.view;
-
-import java.sql.Connection;
-
-public interface Query {
-  QueryResponse getResponse (Connection connection, int connectionType);
-}
