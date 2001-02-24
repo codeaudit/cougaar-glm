c4ef201c46b109697c8a394ffeff866e937ff104
gvidaver <gvidaver>
2001-02-24 20:48:00 +0000

    added SEND_LINES constant

--- glm/src/org/cougaar/domain/mlm/ui/newtpfdd/TPFDDConstants.java
+++ glm/src/org/cougaar/domain/mlm/ui/newtpfdd/TPFDDConstants.java
@@ -8,6 +8,7 @@ public interface TPFDDConstants {
   public String SEND_ASSETID_TD_MAPS = "Send AssetID To Transit Data Maps";
   public String SEND_UNIT_MANIFESTS = "Send unitManifests";
   public String RELOAD = "RELOAD yourself";
+  public String SEND_LINES = "Send lines";
   public final static String ROOTNAME = "ROOT";
 
 }
