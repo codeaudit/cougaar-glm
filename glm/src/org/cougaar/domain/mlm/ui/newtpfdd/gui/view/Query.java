e413215e220c194f61dc460a83e8a079ef55930d
gvidaver <gvidaver>
2001-02-24 20:52:39 +0000

    added getResponse method -- made tentative decision that queries should know how to select things from the database and the rows returned into nodes

--- glm/src/org/cougaar/domain/mlm/ui/newtpfdd/gui/view/Query.java
+++ glm/src/org/cougaar/domain/mlm/ui/newtpfdd/gui/view/Query.java
@@ -1,5 +1,7 @@
-
 package org.cougaar.domain.mlm.ui.newtpfdd.gui.view;
 
+import java.sql.Connection;
+
 public interface Query {
+  QueryResponse getResponse (Connection connection, int connectionType);
 }
