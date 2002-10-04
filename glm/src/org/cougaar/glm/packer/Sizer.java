5b19d016ad4ea019927cd7c13b5dbc6dc84dd865
ngivler <ngivler>
2002-10-04 13:50:25 +0000

    Fix typo.

--- glm/src/org/cougaar/glm/packer/Sizer.java
+++ glm/src/org/cougaar/glm/packer/Sizer.java
@@ -209,7 +209,7 @@ class Sizer {
       Mass massPerEach = assetToBePacked.getPhysicalPG().getMass();
       double taskWeight = task.getPreferredValue(AspectType.QUANTITY) *
 	massPerEach.getShortTons();
-      if (logging.defaultLogger().debugEnabled()) {
+      if (Logging.defaultLogger().debugEnabled()) {
 	Logging.defaultLogger().debug("Sizer.getTaskMass: Quantity: " + 
 				      task.getPreferredValue(AspectType.QUANTITY) + 
 				      " * massPerEach: " + massPerEach.getShortTons() +
