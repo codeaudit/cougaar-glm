84d2be8b9e3d130d08841858417fa364d7a146d0
ahelsing <ahelsing>
2004-03-11 17:09:47 +0000

    Bug 3519: 1) Use separate variable to collect the (possibly new) start and end times. This avoids having the start or end time ever be set to a completely bogus value 2) Synchronize over the updating of start/end time, so they both get updated together 3) Add new start/end time accessor that is synchronized, and returns a TimeSpan. This guarantees that the start and end times you see will be consistent. 4) Note on getStartTime and getEndTime. Since these are not synchronized, any caller that will later compare Start and End Time in any way may get inconsistent answers -- unSAFE. In particular GLMDecorationPlugin, LogisticsOPlan, SubsistenceConsumerBG, DemandForecastPlugin, DemandGeneratorPlugin, InventoryPlugin, DemandTaskGenerator, GenerateProjectionsExpeander, AllocationAssessor, RefillGenerator, RefillProjectionsGenerator, ReconcileSupplyExpander, and SupplyExpander may all need to be updated.

--- glm/src/org/cougaar/glm/plugins/ClusterOPlan.java
+++ glm/src/org/cougaar/glm/plugins/ClusterOPlan.java
@@ -24,6 +24,8 @@ import java.io.Serializable;
 import java.util.Enumeration;
 import java.util.Vector;
 
+import org.cougaar.util.TimeSpan;
+import org.cougaar.util.MutableTimeSpan;
 import org.cougaar.core.blackboard.IncrementalSubscription;
 import org.cougaar.core.mts.MessageAddress;
 import org.cougaar.core.util.UID;
@@ -31,154 +33,180 @@ import org.cougaar.glm.ldm.oplan.Oplan;
 import org.cougaar.glm.ldm.oplan.OrgActivity;
 import org.cougaar.glm.ldm.plan.GeolocLocation;
 
+/**
+ * Object for holding meta-data about the OPlan on the blackboard.
+ **/
 public class ClusterOPlan implements Serializable {
-    MessageAddress clusterId_;
-    long startTime_, endTime_;
-    Vector orgActivities_ = null;
-    Oplan oplan_;
-//      IncrementalSubscription orgActivitySubscription_;
+  MessageAddress clusterId_;
+  long startTime_, endTime_;
+  Vector orgActivities_ = null;
+  Oplan oplan_;
+  //      IncrementalSubscription orgActivitySubscription_;
 
-    public ClusterOPlan(MessageAddress id, Oplan op/*, IncrementalSubscription sub*/) {
-//  	System.out.println("--- Creating ClusterOPlan for "+id+", oplan "+op);
-	clusterId_ = id;
-	oplan_ = op;
-	startTime_ = oplan_.getCday().getTime();
-	endTime_ = oplan_.getCday().getTime();
-//  	orgActivitySubscription_ = sub;
-//  	updateOrgActivities(orgActivitySubscription_.elements());
-//  	updateOPlanTimes();
+  public ClusterOPlan(MessageAddress id, Oplan op/*, IncrementalSubscription sub*/) {
+    //  	System.out.println("--- Creating ClusterOPlan for "+id+", oplan "+op);
+    clusterId_ = id;
+    oplan_ = op;
+    startTime_ = oplan_.getCday().getTime();
+    endTime_ = oplan_.getCday().getTime();
+    //  	orgActivitySubscription_ = sub;
+    //  	updateOrgActivities(orgActivitySubscription_.elements());
+    //  	updateOPlanTimes();
+  }
+
+  /* If OPlan does not change but OrgActivities for the OPlan change
+   * then get the updated OrgActivities.
+   */
+  public boolean updateOrgActivities(IncrementalSubscription orgActivitySubscription_) {
+    // Only update OrgActivities if subscription has changed
+    if (orgActivitySubscription_.getChangedList().hasMoreElements()
+	|| orgActivitySubscription_.getAddedList().hasMoreElements() 
+	|| orgActivitySubscription_.getRemovedList().hasMoreElements()) {
+      //  	    System.out.println("--- New/Changed/Removed OrgActivities for "+clusterId_);
+      updateOrgActivities(orgActivitySubscription_.elements());
+      updateOPlanTimes();
+      return true;
+    }
+    return false;
+  }
+
+  // Re-calculate the overall start and end times for the OPlan
+  private void updateOPlanTimes() {
+    long end_time;
+    long newET = Long.MIN_VALUE;
+    long start_time;
+    long newST = Long.MAX_VALUE;
+    OrgActivity activity;
+    Enumeration activities = orgActivities_.elements();
+
+    // initialize endTime/startTime values
+    if (activities.hasMoreElements()) {
+      activity = (OrgActivity)activities.nextElement();
+      newET = getEndTime(activity);
+      newST = getStartTime(activity);
     }
 
-    /* If OPlan does not change but OrgActivities for the OPlan change
-     * then get the updated OrgActivities.
-     */
-    public boolean updateOrgActivities(IncrementalSubscription orgActivitySubscription_) {
-	// Only update OrgActivities if subscription has changed
-	if (orgActivitySubscription_.getChangedList().hasMoreElements()
-	    || orgActivitySubscription_.getAddedList().hasMoreElements() 
-	    || orgActivitySubscription_.getRemovedList().hasMoreElements()) {
-//  	    System.out.println("--- New/Changed/Removed OrgActivities for "+clusterId_);
-	    updateOrgActivities(orgActivitySubscription_.elements());
-	    updateOPlanTimes();
-	    return true;
-	}
-	return false;
+    // search for first/last times
+    while (activities.hasMoreElements()) {
+      activity = (OrgActivity)activities.nextElement();
+      end_time = getEndTime(activity);
+      if ((end_time > newET)) {
+	newET = end_time;
+      }
+      start_time = getStartTime(activity);
+      if (start_time < newST) {
+	newST = start_time;
+      }
     }
 
-    private void updateOPlanTimes() {
-	long end_time;
-	long start_time;
-	OrgActivity activity;
-	Enumeration activities = orgActivities_.elements();
-
-	// initialize endTime/startTime values
-	if (activities.hasMoreElements()) {
-	    activity = (OrgActivity)activities.nextElement();
-	    endTime_ = getEndTime(activity);
-	    startTime_ = getStartTime(activity);
-	}
-	// search for first/last times
-	while (activities.hasMoreElements()) {
-	    activity = (OrgActivity)activities.nextElement();
-	    end_time = getEndTime(activity);
-	    if ((end_time > endTime_)) {
-		endTime_ = end_time;
-	    }
-	    start_time = getStartTime(activity);
-	    if (start_time < startTime_) {
-		startTime_ = start_time;
-	    }
-	}
+    if (newET != endTime_ || newST != startTime_) {
+      //System.out.println("ClusterOplan at " + clusterId_ + " updating startTime from " + startTime_ + " to " + newST + ", and endTime from " + endTime_ + " to " + newET);
+      synchronized(this) {
+	endTime_ = newET;
+	startTime_ = newST;
+      }
     }
+  }
 
-    public void updateOrgActivities(Enumeration activities) {
-	orgActivities_ = new Vector();
-	OrgActivity orgact;
-	String cluster_name = clusterId_.toString();
-	while (activities.hasMoreElements()) {
-	    // only deal w/ org activities for this cluster
-	    orgact = (OrgActivity)activities.nextElement();
-	    if (orgact.getOrgID().equals(cluster_name)) {
-//  		System.out.println("--- Adding OrgActivity for "+clusterId_+", activity "+orgact.getActivityName()+
-//  				   ", "+orgact.getOpTempo()+", "+orgact);
-		orgActivities_.add(orgact);
-	    }
-	}
+  public void updateOrgActivities(Enumeration activities) {
+    orgActivities_ = new Vector();
+    OrgActivity orgact;
+    String cluster_name = clusterId_.toString();
+    while (activities.hasMoreElements()) {
+      // only deal w/ org activities for this cluster
+      orgact = (OrgActivity)activities.nextElement();
+      if (orgact.getOrgID().equals(cluster_name)) {
+	//  		System.out.println("--- Adding OrgActivity for "+clusterId_+", activity "+orgact.getActivityName()+
+	//  				   ", "+orgact.getOpTempo()+", "+orgact);
+	orgActivities_.add(orgact);
+      }
     }
+  }
 
-    /* getOplan() returns the OPlan UID this object is handling
-     */
-    public UID getOplanUID() {
-	return oplan_.getUID();
-    }
+  /* getOplan() returns the OPlan UID this object is handling
+   */
+  public UID getOplanUID() {
+    return oplan_.getUID();
+  }
 
-    public long getEndTime(OrgActivity act) {
-	return act.getTimeSpan().getEndDate().getTime();
-    }
+  public long getEndTime(OrgActivity act) {
+    return act.getTimeSpan().getEndDate().getTime();
+  }
 
-    public long getStartTime(OrgActivity act) {
-	return act.getTimeSpan().getStartDate().getTime();
-    }
+  public long getStartTime(OrgActivity act) {
+    return act.getTimeSpan().getStartDate().getTime();
+  }
 
-    /* Latest end time of the OrgActivities
-     * If now orgActivities are received, end time is not set
-     */
-    public long getEndTime() {
-	return endTime_;
-    }
+  /* Latest end time of the OrgActivities
+   * If no orgActivities are received, end time is not set
+   * <b>WARN</b>: This is not safe, particularly if you will be comparing to StartTime. Use #getOplanSpan()
+   */
+  public long getEndTime() {
+    return endTime_;
+  }
 
-    /* Earliest start time of the OrgActivities
-     * If now orgActivities are received, start time is not set
-     */
-    public long getStartTime() {
-	return startTime_;
-    }
+  /* Earliest start time of the OrgActivities
+   * If no orgActivities are received, start time is not set
+   * <b>WARN</b>: This is not safe, particularly if you will be comparing to EndTime. Use #getOplanSpan()
+   */
+  public long getStartTime() {
+    return startTime_;
+  }
 
-    public GeolocLocation getGeoLoc(long time) {
-	OrgActivity oa = getOrgActivity(time);
-	if (oa != null) {
-	    return oa.getGeoLoc();
-	}
-	return null;
-    }
+  /**
+   * Return the current start and end times (longs) of the OPlan
+   **/
+  public synchronized TimeSpan getOplanSpan() {
+    MutableTimeSpan ts = new MutableTimeSpan();
+    ts.setTimeSpan(startTime_, endTime_);
+    return ts;
+  }
 
-    public String getOpTempo(long time) {
-	OrgActivity oa = getOrgActivity(time);
-	if (oa != null) {
-	    return oa.getOpTempo();
-	}
-	return null;
+  public GeolocLocation getGeoLoc(long time) {
+    OrgActivity oa = getOrgActivity(time);
+    if (oa != null) {
+      return oa.getGeoLoc();
     }
+    return null;
+  }
+
+  public String getOpTempo(long time) {
+    OrgActivity oa = getOrgActivity(time);
+    if (oa != null) {
+      return oa.getOpTempo();
+    }
+    return null;
+  }
   
-    public long getOplanCday() {
-      return  oplan_.getCday().getTime();
-    } 
+  public long getOplanCday() {
+    return  oplan_.getCday().getTime();
+  } 
     
-    public OrgActivity getOrgActivity(long t) {
-	long end_time, start_time;
-	Enumeration enum = orgActivities_.elements();
-	OrgActivity oa;
-	while (enum.hasMoreElements()) {
-	    oa = (OrgActivity)enum.nextElement();
-	    start_time = getStartTime(oa);
-	    end_time = getEndTime(oa);
-	    if ((t >= start_time) && (t < end_time)) {
-		return oa;
-	    }
-	}
-	return null;
+  public OrgActivity getOrgActivity(long t) {
+    long end_time, start_time;
+    Enumeration enum = orgActivities_.elements();
+    OrgActivity oa;
+    while (enum.hasMoreElements()) {
+      oa = (OrgActivity)enum.nextElement();
+      start_time = getStartTime(oa);
+      end_time = getEndTime(oa);
+      if ((t >= start_time) && (t < end_time)) {
+	return oa;
+      }
     }
+    return null;
+  }
 
-//      /* When disposing of a ClusterOPlan object, need to get the
-//       * OrgActivity subscription to do an 'unsubscribe'
-//       */
-//      public IncrementalSubscription getOrgActivitySubscription() {
-//  	return orgActivitySubscription_;
-//      }
+  //      /* When disposing of a ClusterOPlan object, need to get the
+  //       * OrgActivity subscription to do an 'unsubscribe'
+  //       */
+  //      public IncrementalSubscription getOrgActivitySubscription() {
+  //  	return orgActivitySubscription_;
+  //      }
 
-    public String toString() {
-	return oplan_.toString();
-    }
+  public String toString() {
+    return oplan_.toString();
+  }
 }
 
 
