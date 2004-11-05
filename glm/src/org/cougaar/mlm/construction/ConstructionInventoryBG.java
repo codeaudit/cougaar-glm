5219c3a04d6471ca5a804d69d01282950b5247a1
lgoldsto <lgoldsto>
2004-11-05 23:26:10 +0000

    Removed GLMDebug logger and replaced with LoggingService logger. Some other minor cleanup. Bug #3452

--- glm/src/org/cougaar/mlm/construction/ConstructionInventoryBG.java
+++ glm/src/org/cougaar/mlm/construction/ConstructionInventoryBG.java
@@ -24,52 +24,57 @@
  * </copyright>
  * --------------------------------------------------------------------------*/
 package org.cougaar.mlm.construction;
-import org.cougaar.glm.debug.GLMDebug;
+
 import org.cougaar.glm.ldm.asset.InventoryBG;
 import org.cougaar.glm.ldm.asset.InventoryPG;
 import org.cougaar.glm.ldm.asset.NewInventoryPG;
 import org.cougaar.planning.ldm.asset.PGDelegate;
 import org.cougaar.planning.ldm.asset.PropertyGroup;
 import org.cougaar.planning.ldm.measure.Count;
+import org.cougaar.util.log.Logger;
+import org.cougaar.util.log.Logging;
 
-public  class ConstructionInventoryBG extends InventoryBG {
+public class ConstructionInventoryBG extends InventoryBG {
+  protected Logger logger = Logging.getLogger(ConstructionInventoryBG.class);
 
-    public ConstructionInventoryBG(InventoryPG pg) {
-	    super(pg);
+  public ConstructionInventoryBG(InventoryPG pg) {
+    super(pg);
+  }
+
+  public boolean initialize(double[] levels) {
+
+    try {
+      double capacity = levels[0];
+      double erq = levels[2];
+      ((NewInventoryPG) myPG_).setCapacity(new Count(levels[0], Count.EACHES));
+      ((NewInventoryPG) myPG_).setInitialLevel(new Count(levels[1], Count.EACHES));
+      ((NewInventoryPG) myPG_).setReorderLevel(new Count(levels[2], Count.EACHES));
+      ((NewInventoryPG) myPG_).setMinReorder(new Count(levels[3], Count.EACHES));
+      ((NewInventoryPG) myPG_).setUnobtainable(new Count(0.0, Count.EACHES));
+      ((NewInventoryPG) myPG_).setFillToCapacity(false);
+
+      ((NewInventoryPG) myPG_).setMaintainAtCapacity(false);
+
+      if ((capacity < erq) || (erq < 0)) {
+        if (logger.isErrorEnabled()) {
+          logger.error("Cannot create InventoryBG, capacity " + capacity + " < erq " + erq);
+        }
+        initialized_ = false;
+      } else {
+        initialized_ = true;
+        if (logger.isDebugEnabled()) {
+          logger.debug("Initialized InventoryPG, capacity " + capacity + ", erq " + erq + ", initiali level: " + levels[1] + ", min reorder: " + levels[3]);
+        }
+      }
+    } catch (NullPointerException npe) {
+      initialized_ = false;
     }
-    
-    public boolean initialize(double[] levels) {
+    return initialized_;
 
-	  try {
-	    double capacity = levels[0];
-	    double erq = levels[2];
-	    ((NewInventoryPG)myPG_).setCapacity(new Count(levels[0], Count.EACHES));
-	    ((NewInventoryPG)myPG_).setInitialLevel(new Count(levels[1], Count.EACHES));
-	    ((NewInventoryPG)myPG_).setReorderLevel(new Count(levels[2], Count.EACHES));
-	    ((NewInventoryPG)myPG_).setMinReorder(new Count(levels[3], Count.EACHES));
-	    ((NewInventoryPG)myPG_).setUnobtainable(new Count(0.0, Count.EACHES));
-	    ((NewInventoryPG)myPG_).setFillToCapacity(false);
-
-	    ((NewInventoryPG)myPG_).setMaintainAtCapacity(false);
-
-	    if ((capacity < erq) || (erq < 0)) {
-		    GLMDebug.ERROR("ConstructionInventoryBG","Cannot create InventoryBG, capacity "+
-				 capacity+" < erq "+erq);
-		    initialized_ = false;
-	    } else {
-		    initialized_ = true;
-		    GLMDebug.DEBUG("ConstructionInventoryBG","Initialized InventoryPG, capacity "+
-				capacity+", erq "+erq+", initiali level: "+levels[1]+", min reorder: "+levels[3]);
-	    }
-	  } catch (NullPointerException npe) {
-	    initialized_ = false;
-	  }
-	  return initialized_;
-	
   }
 
   public PGDelegate copy(PropertyGroup pg) {
-	  ConstructionInventoryBG bg = new ConstructionInventoryBG((InventoryPG)pg);
-	  return bg;
+    ConstructionInventoryBG bg = new ConstructionInventoryBG((InventoryPG) pg);
+    return bg;
   }
 }
