c5687c9ecdade803ec8a3e874d0393cb0b951254
gvidaver <gvidaver>
2005-08-11 19:56:02 +0000

    Added enhancement submitted in bug #3838

--- toolkit/src/org/cougaar/lib/xml/parser/InstanceParser.java
+++ toolkit/src/org/cougaar/lib/xml/parser/InstanceParser.java
@@ -28,6 +28,7 @@ package org.cougaar.lib.xml.parser;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 import org.cougaar.lib.util.UTILAsset;
 import org.cougaar.planning.ldm.LDMServesPlugin;
@@ -46,49 +47,57 @@ import org.w3c.dom.NodeList;
  */
 public class InstanceParser{
 
-  public InstanceParser (Logger logger) { 
-    scheduleParser = new ScheduleParser(); 
+  public InstanceParser (Logger logger) {
+    scheduleParser = new ScheduleParser();
+    fieldParser    = new FieldParser(logger, new ObjectParser(logger));
     assetHelper    = new UTILAsset(logger);
   }
 
   public List getInstance(LDMServesPlugin ldm, Node node){
 
-    Schedule newSchedule = null; 
+    Schedule newSchedule = null;
     List newAssets = new ArrayList ();
+    List fields = new ArrayList ();
 
     if(node.getNodeName().equals("instance")){
-      NodeList  nlist    = node.getChildNodes();      
+      NodeList  nlist    = node.getChildNodes();
       int       nlength  = nlist.getLength();
 
       String id        = node.getAttributes().getNamedItem("id").getNodeValue();
       String prototype = node.getAttributes().getNamedItem("prototype").getNodeValue();
-      
+
       // Only expect one schedule per instance but what the heck.
       for(int i = 0; i < nlength; i++) {
-	Node    child       = nlist.item(i);
-	if(child.getNodeType() == Node.ELEMENT_NODE) {
-	  if(child.getNodeName().equals("schedule")){
-	    newSchedule = scheduleParser.getSchedule(ldm, child);
-	  }
-	}
+        Node    child       = nlist.item(i);
+        if(child.getNodeType() == Node.ELEMENT_NODE) {
+          if(child.getNodeName().equals("schedule")){
+            newSchedule = scheduleParser.getSchedule(ldm, child);
+          }
+          else if(child.getNodeName().equals("field"))
+          {
+            // We may have to set the field on a bunch of instances.
+            // So, we just save a list for now.
+            fields.add(child);
+          }
+        }
       }
 
       Integer intQ = new Integer (1);
 
       if (node.getAttributes().getNamedItem("quantity") != null) {
-	String quantity = node.getAttributes().getNamedItem("quantity").getNodeValue();
-	intQ = new Integer(quantity);
+        String quantity = node.getAttributes().getNamedItem("quantity").getNodeValue();
+        intQ = new Integer(quantity);
       }
 
       for (int j = 0; j < intQ.intValue(); j++) {
-	String newID = id;
-	String prefix = "";
-	// we want id's less than 10 to be 01, 02, etc. instead of 0,1,2
-	if (intQ.intValue () > 1) {
-	  prefix = (j < 9) ? "0" : "";
-	  newID = newID + "-" + prefix+(j+1);
-	}
-	newAssets.add (makeNewAsset(ldm, prototype, newID, newSchedule));
+        String newID = id;
+        String prefix = "";
+        // we want id's less than 10 to be 01, 02, etc. instead of 0,1,2
+        if (intQ.intValue () > 1) {
+          prefix = (j < 9) ? "0" : "";
+          newID = newID + "-" + prefix+(j+1);
+        }
+        newAssets.add (makeNewAsset(ldm, prototype, newID, newSchedule, fields));
       }
 
       return newAssets;
@@ -105,20 +114,33 @@ public class InstanceParser{
    * @param prototype what kind of asset is it
    * @param id unique identifier within kind
    * @param newSchedule initial availability
+   * @param fields A List of "Node", extracted from fields,
+   *               probably property groups. Set to null to
+   *               not set any additional fields.
    * @return the new asset!
    */
-  protected Asset makeNewAsset (LDMServesPlugin ldm, 
-				String prototype, 
-				String id, 
-				Schedule newSchedule) {
+  protected Asset makeNewAsset (LDMServesPlugin ldm,
+                                String prototype,
+                                String id,
+                                Schedule newSchedule,
+                                List fields) {
     // Create the instance
     Asset newAsset = assetHelper.createInstance(ldm,
-						prototype, id );
+                                                prototype, id );
 
 //    Schedule copySchedule = ldm.getFactory().newSimpleSchedule(new Date(newSchedule.getStartTime()),
 //							       new Date(newSchedule.getEndTime()));
     // Set the Schedule
     ((NewRoleSchedule)newAsset.getRoleSchedule()).setAvailableSchedule(newSchedule);
+
+    if (null != fields) {
+      for(Iterator it = fields.iterator(); it.hasNext(); ) {
+        Node n = (Node) it.next();
+        //  FieldParser seems to handle overwriting propertygroupps
+        //  just fine, though it may merit further investigation.
+        fieldParser.setField(ldm, n, newAsset);
+      }
+    }
 
     //logger.debug ("Making new asset " + newAsset + " with UID " + 
     //newAsset.getUID());
@@ -126,6 +148,7 @@ public class InstanceParser{
   }
 
   protected ScheduleParser scheduleParser;
+  protected FieldParser fieldParser;
   protected UTILAsset assetHelper;
 }
 
